fa6696cff875c3047ac1aa0f97dbf3ee054e3eed
Kieran <kieranh@web.de>
2023-11-07 10:53:12 +0000

    Init

---
+++ V5_3/src/v5_3/V5_3.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package v5_3;
+
+/**
+ *
+ * @author Kieran
+ */
+public class V5_3 
+{
+  public V5_3()
+  {
+  //test
+  }
+
+  public static void main(String[] args) 
+  {
+    new V5_3();
+  }
+
+}
